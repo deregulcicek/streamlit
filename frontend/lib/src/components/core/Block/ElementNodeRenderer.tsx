534e5ce8cc0b3423c397d40c7ad6259d33444200
Karen Javadyan <kajarenc@gmail.com>
2024-07-29 19:40:01 +0400

    add frontend scaffolding

--- frontend/lib/src/components/core/Block/ElementNodeRenderer.tsx
+++ frontend/lib/src/components/core/Block/ElementNodeRenderer.tsx
@@ -582,6 +582,7 @@ const RawElementNodeRenderer = (
         <ChatInput
           key={chatInputProto.id}
           element={chatInputProto}
+          uploadClient={props.uploadClient}
           {...widgetProps}
         />
       )
