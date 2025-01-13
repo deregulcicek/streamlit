3688198a6a177ad1682b15fb5fe638d01c86c9d9
Laura Wilby <laura.wilby@snowflake.com>
2025-01-13 16:29:35 +0100

    Update headers to use new license including 2025

--- component-lib/babel.config.js
+++ component-lib/babel.config.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2024)
+ * Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2025)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,8 @@
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
-    ['@babel/preset-react', {runtime: 'automatic'}],
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+    ["@babel/preset-react", { runtime: "automatic" }],
   ],
 };
