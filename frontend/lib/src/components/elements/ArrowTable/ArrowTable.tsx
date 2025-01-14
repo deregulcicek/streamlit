067991a9bb9b1dcf30fa46beaa41f622b988ccb6
lukasmasuch <lukas.masuch@gmail.com>
2025-01-14 19:07:16 +0100

    Fix usage of caption

--- frontend/lib/src/components/elements/ArrowTable/ArrowTable.tsx
+++ frontend/lib/src/components/elements/ArrowTable/ArrowTable.tsx
@@ -30,6 +30,7 @@ import {
   StyledEmptyTableCell,
   StyledTable,
   StyledTableBorder,
+  StyledTableCaption,
   StyledTableCell,
   StyledTableCellHeader,
   StyledTableContainer,
@@ -72,13 +73,13 @@ export function ArrowTable(props: Readonly<TableProps>): ReactElement {
         </StyledTable>
       </StyledTableBorder>
       {/* One negative side effect of having the border on a wrapper is that we need
-      to put the caption outside of <table>, so it shows up outside of the border. This
-      is not great for accessibility. But I think it's fine because adding captions
+      to put the caption outside of <table> and use a div, so it shows up outside of the border.
+      This is not great for accessibility. But I think it's fine because adding captions
       isn't a native feature (you can only do it via Pandas Styler's `set_caption`
       function) and I couldn't find a single example on GitHub that actually does this
       for `st.table`. We might want to revisit this if we add captions/labels as a
       native feature or do a pass on accessibility. */}
-      {caption && <caption>{caption}</caption>}
+      {caption && <StyledTableCaption>{caption}</StyledTableCaption>}
     </StyledTableContainer>
   )
 }
