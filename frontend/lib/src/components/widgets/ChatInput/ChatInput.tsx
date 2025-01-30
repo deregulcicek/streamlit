7adce0540b9bbff6ebe99daf1bd133d1b77ca214
Eric Chiu <e.chiu@snowflake.com>
2025-01-29 17:49:10 -0800

    Split `FileUploadArea` and handlers out from `ChatInput` (#10291)
    ## Describe your changes
    
    as title -- refactored to keep `ChatInput` leaner
    
    ## GitHub Issue Link (if applicable)
    
    ## Testing Plan
    
    - Explanation of why no additional tests are needed
    - Unit Tests (JS and/or Python)
    - E2E Tests
    - Any manual testing needed?
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- frontend/lib/src/components/widgets/ChatInput/ChatInput.tsx
+++ frontend/lib/src/components/widgets/ChatInput/ChatInput.tsx
@@ -23,38 +23,28 @@ import React, {
   useState,
 } from "react"
 
-import axios from "axios"
 import { useTheme } from "@emotion/react"
 import { Send } from "@emotion-icons/material-rounded"
 import { Textarea as UITextArea } from "baseui/textarea"
-import { AttachFile } from "@emotion-icons/material-outlined"
-import {
-  ErrorCode as FileErrorCode,
-  FileRejection,
-  useDropzone,
-} from "react-dropzone"
-import zip from "lodash/zip"
+import { useDropzone } from "react-dropzone"
 
 import {
   ChatInput as ChatInputProto,
   FileUploaderState as FileUploaderStateProto,
-  FileURLs as FileURLsProto,
   IChatInputValue,
   IFileURLs,
   UploadedFileInfo as UploadedFileInfoProto,
 } from "@streamlit/protobuf"
 
-import { EmotionTheme } from "~lib/theme"
 import {
   AcceptFileValue,
   chatInputAcceptFileProtoValueToEnum,
   isNullOrUndefined,
 } from "~lib/util/utils"
-import { WidgetInfo, WidgetStateManager } from "~lib/WidgetStateManager"
+import { WidgetStateManager } from "~lib/WidgetStateManager"
 import Icon from "~lib/components/shared/Icon"
 import InputInstructions from "~lib/components/shared/InputInstructions/InputInstructions"
 import { isEnterKeyPressed } from "~lib/util/inputUtils"
-import BaseButton, { BaseButtonKind } from "~lib/components/shared/BaseButton"
 import {
   UploadedStatus,
   UploadFileInfo,
@@ -64,13 +54,14 @@ import { FileUploadClient } from "~lib/FileUploadClient"
 import {
   StyledChatInput,
   StyledChatInputContainer,
-  StyledFileUploadDropzone,
   StyledInputInstructionsContainer,
   StyledSendIconButton,
   StyledSendIconButtonContainer,
-  StyledVerticalDivider,
 } from "./styled-components"
-import ChatUploadedFiles from "./UploadedFile/ChatUploadedFiles"
+import ChatUploadedFiles from "./fileUpload/ChatUploadedFiles"
+import FileUploadArea from "./fileUpload/FileUploadArea"
+import { createUploadFileHandler } from "./fileUpload/createFileUploadHandler"
+import { createDropHandler } from "./fileUpload/createDropHandler"
 
 export interface Props {
   disabled: boolean
@@ -81,197 +72,12 @@ export interface Props {
   fragmentId?: string
 }
 
-interface CreateDropHandlerParams {
-  acceptMultipleFiles: boolean
-  uploadClient: FileUploadClient
-  uploadFile: (fileURLs: FileURLsProto, file: File) => void
-  addFiles: (files: UploadFileInfo[]) => void
-  getNextLocalFileId: () => number
-  deleteExistingFiles: () => void
-  onUploadComplete: () => void
-}
-
-const createDropHandler =
-  ({
-    acceptMultipleFiles,
-    uploadClient,
-    uploadFile,
-    addFiles,
-    getNextLocalFileId,
-    deleteExistingFiles,
-    onUploadComplete,
-  }: CreateDropHandlerParams) =>
-  (acceptedFiles: File[], rejectedFiles: FileRejection[]): void => {
-    // If only single file upload is allowed but multiple were dropped/selected,
-    // all files will be rejected by default. In this case, we take the first
-    // valid file into acceptedFiles, and reject the rest.
-    if (
-      !acceptMultipleFiles &&
-      acceptedFiles.length === 0 &&
-      rejectedFiles.length > 1
-    ) {
-      const firstFileIndex = rejectedFiles.findIndex(
-        file => file.errors?.[0].code === FileErrorCode.TooManyFiles
-      )
-
-      if (firstFileIndex >= 0) {
-        acceptedFiles.push(rejectedFiles[firstFileIndex].file)
-        rejectedFiles.splice(firstFileIndex, 1)
-      }
-    }
-
-    if (!acceptMultipleFiles && acceptedFiles.length > 0) {
-      deleteExistingFiles()
-    }
-
-    uploadClient
-      .fetchFileURLs(acceptedFiles)
-      .then((fileURLsArray: IFileURLs[]) => {
-        zip(fileURLsArray, acceptedFiles).forEach(
-          ([fileURLs, acceptedFile]) => {
-            uploadFile(fileURLs as FileURLsProto, acceptedFile as File)
-          }
-        )
-      })
-      .catch((errorMessage: string) => {
-        addFiles(
-          acceptedFiles.map(f => {
-            return new UploadFileInfo(f.name, f.size, getNextLocalFileId(), {
-              type: "error",
-              errorMessage,
-            })
-          })
-        )
-      })
-
-    // Create an UploadFileInfo for each of our rejected files, and add them to
-    // our state.
-    if (rejectedFiles.length > 0) {
-      const rejectedInfos = rejectedFiles.map(
-        rejected =>
-          new UploadFileInfo(
-            rejected.file.name,
-            rejected.file.size,
-            getNextLocalFileId(),
-            {
-              type: "error",
-              errorMessage: rejected.errors
-                .map(err => err.message)
-                .filter(err => !!err)
-                .join(", "),
-            }
-          )
-      )
-      addFiles(rejectedInfos)
-    }
-
-    onUploadComplete()
-  }
-
-interface CreateUploadFileParams {
-  getNextLocalFileId: () => number
-  addFiles: (files: UploadFileInfo[]) => void
-  updateFile: (id: number, fileInfo: UploadFileInfo) => void
-  uploadClient: FileUploadClient
-  element: WidgetInfo
-  onUploadProgress: (e: ProgressEvent, id: number) => void
-  onUploadComplete: (id: number, fileURLs: IFileURLs) => void
-}
-
-const createUploadFileHandler =
-  ({
-    getNextLocalFileId,
-    addFiles,
-    updateFile,
-    uploadClient,
-    element,
-    onUploadProgress,
-    onUploadComplete,
-  }: CreateUploadFileParams) =>
-  (fileURLs: IFileURLs, file: File): void => {
-    // Create an UploadFileInfo for this file and add it to our state.
-    const cancelToken = axios.CancelToken.source()
-    const uploadingFileInfo = new UploadFileInfo(
-      file.name,
-      file.size,
-      getNextLocalFileId(),
-      {
-        type: "uploading",
-        cancelToken,
-        progress: 1,
-      }
-    )
-    addFiles([uploadingFileInfo])
-
-    uploadClient
-      .uploadFile(
-        {
-          formId: "", // TODO[kajarnec] fix this probably with uploadFile refactoring
-          ...element,
-        },
-        fileURLs.uploadUrl as string,
-        file,
-        e => onUploadProgress(e, uploadingFileInfo.id),
-        cancelToken.token
-      )
-      .then(() => onUploadComplete(uploadingFileInfo.id, fileURLs))
-      .catch(err => {
-        // If this was a cancel error, we don't show the user an error -
-        // the cancellation was in response to an action they took.
-        if (!axios.isCancel(err)) {
-          updateFile(
-            uploadingFileInfo.id,
-            uploadingFileInfo.setStatus({
-              type: "error",
-              errorMessage: err ? err.toString() : "Unknown error",
-            })
-          )
-        }
-      })
-  }
-
 // We want to show easily that there's scrolling so we deliberately choose
 // a half size.
 const MAX_VISIBLE_NUM_LINES = 6.5
 // Rounding errors can arbitrarily create scrollbars. We add a rounding offset
 // to manage it better.
 const ROUNDING_OFFSET = 1
-
-interface FileUploadAreaProps {
-  getRootProps: any
-  getInputProps: any
-  showDropzone: boolean
-  disabled: boolean
-  theme: EmotionTheme
-}
-
-const FileUploadArea = ({
-  getRootProps,
-  getInputProps,
-  showDropzone,
-  disabled,
-  theme,
-}: FileUploadAreaProps): React.ReactElement =>
-  showDropzone ? (
-    <StyledFileUploadDropzone {...getRootProps()}>
-      <input {...getInputProps()} />
-      Drag and drop files here
-    </StyledFileUploadDropzone>
-  ) : (
-    <>
-      <div data-testid="stChatInputFileUploadButton" {...getRootProps()}>
-        <input {...getInputProps()} />
-        <BaseButton kind={BaseButtonKind.MINIMAL} disabled={disabled}>
-          <Icon
-            content={AttachFile}
-            size="lg"
-            color={theme.colors.fadedText60}
-          />
-        </BaseButton>
-      </div>
-      <StyledVerticalDivider />
-    </>
-  )
 
 function ChatInput({
   width,
@@ -375,12 +181,12 @@ function ChatInput({
     uploadFile: createUploadFileHandler({
       getNextLocalFileId,
       addFiles,
-      updateFile: (id, fileInfo) => {
+      updateFile: (id: number, fileInfo: UploadFileInfo) => {
         setFiles(files => updateFile(id, fileInfo, files))
       },
       uploadClient,
       element,
-      onUploadProgress: (e, fileId) => {
+      onUploadProgress: (e: ProgressEvent, fileId: number) => {
         setFiles(files => {
           const file = getFile(fileId, files)
           if (isNullOrUndefined(file) || file.status.type !== "uploading") {
@@ -403,7 +209,7 @@ function ChatInput({
           )
         })
       },
-      onUploadComplete: (id, fileUrls) => {
+      onUploadComplete: (id: number, fileUrls: IFileURLs) => {
         setFiles(files => {
           const curFile = getFile(id, files)
           if (
