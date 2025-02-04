b37ea95108041c5755473ad15982954af508a8a3
lukasmasuch <lukas.masuch@gmail.com>
2025-02-04 19:01:32 +0100

    Fix

--- frontend/lib/src/hostComm/HostCommunicationManager.test.tsx
+++ frontend/lib/src/hostComm/HostCommunicationManager.test.tsx
@@ -419,7 +419,7 @@ describe("HostCommunicationManager messaging", () => {
       backgroundColor: "#FFFFFF",
       secondaryBackgroundColor: "#F5F5F5",
       textColor: "#1A1D21",
-      // Option is deprecated, but we still test to ensure backwards:
+      // Option is deprecated, but we still test to ensure backwards compatibility:
       widgetBorderColor: "#D3DAE8",
       // Option is deprecated, but we still test to ensure backwards:
       widgetBackgroundColor: "#FFFFFF",
