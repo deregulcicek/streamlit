c75395ea1b15b8d17ccf5a9352bdbf0d635fb54c
Laura Wilby <laura.wilby@snowflake.com>
2025-02-25 11:36:35 +0100

    use LOG

--- frontend/lib/src/components/widgets/NumberInput/utils.ts
+++ frontend/lib/src/components/widgets/NumberInput/utils.ts
@@ -22,7 +22,7 @@ import { isNullOrUndefined, notNullOrUndefined } from "~lib/util/utils"
 
 import { Props } from "./NumberInput"
 
-const log = getLogger("NumberInput")
+const LOG = getLogger("NumberInput")
 
 /**
  * Return a string property from an element. If the string is
@@ -74,7 +74,7 @@ export const formatValue = ({
   try {
     return sprintf(formatString, value)
   } catch (e) {
-    log.warn(`Error in sprintf(${formatString}, ${value}): ${e}`)
+    LOG.warn(`Error in sprintf(${formatString}, ${value}): ${e}`)
     return String(value)
   }
 }
